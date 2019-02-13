55b09de0e1163b99531f7491df31061c17a3bd9e
Florian Weber <florian.weber@live.de>
2019-02-13 11:32:07 +0100

    nomsg

--- buildCORE.sh
+++ buildCORE.sh
@@ -1,2 +1,2 @@
-g++ -g -std=c++17 main.cpp DB/DB.cpp IoD/IoD.cpp -o CORE -L /usr/local/lib -lboost_system -pthread
+g++ -g -std=c++17 main.cpp clock.cpp IoD/IoD.cpp mysqlcon.cpp util.cpp ModuleFramework/control.cpp ModuleFramework/module.cpp ModuleFramework/modulemanager.cpp ModuleFramework/timer.cpp REST/REST.cpp REST/eventhandler.cpp -o CORE -L /usr/local/lib -lboost_system -pthread -lmariadbclient
 chmod +x CORE
